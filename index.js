9e5b4108ead57fb05031e4ca052467a000e31b7f
Marvin Fuchs <info@marvin-fuchs.de>
2023-02-27 17:25:06 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// Setup map
+var svgRoot
+var svgMap
+var svgViewport
+
+fetch('/src/map.svg')
+    .then((response) => response.text())
+    .then((svg) => {
+        svgRoot = SVG().addTo('#map_container').size('1920px', '1080px').svg(svg)
+        svgMap = svgRoot.findOne('#Map')
+        svgViewport = svgRoot.findOne('#Viewport').clone()
+        resizeMap();
+    })
+
+addEventListener("resize", resizeMap);
+function resizeMap() {
+    svgMap.move(
+        -svgViewport.x() + (window.innerWidth - svgViewport.width()) / 2,
+        -svgViewport.y() + (window.innerHeight - svgViewport.height()) / 2)
+
+    var xScale = window.innerWidth / svgViewport.width()
+    var yScale = window.innerHeight / svgViewport.height()
+
+    if (xScale < yScale) {
+        $('#map_container').css('transform', 'scale(' + xScale + ')')
+    } else {
+        $('#map_container').css('transform', 'scale(' + yScale + ')')
+    }
+}
