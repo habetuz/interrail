e33ce088f61dabbaf0004188d45af47f2ee9b62d
Marvin Fuchs <info@marvin-fuchs.de>
2023-05-16 22:34:44 +0200

    Update site

--- src/js/slideshow.js
+++ src/js/slideshow.js
@@ -1,1 +1,7 @@
-class SlideShow extends HTMLElement{connectedCallback(){var t=this.getAttribute("src"),n=$('<swiper-container loop="true" mousewheel="true" autoplay="true" grab-cursor="true" space-between="50"><\/swiper-container>');$.get("src/img/"+t+"/index.json",t=>{t.forEach(t=>{n.append($(`<swiper-slide><img src='${t}'></img></swiper-slide>`))})});this.appendChild(n[0])}}customElements.define("slide-show",SlideShow)
+class SlideShow extends HTMLElement{connectedCallback(){var t=this.getAttribute("src"),n=$('<swiper-container mousewheel="true" autoplay-delay="2000" grab-cursor="true" space-between="50" pagination-clickable="true" pagination-dynamic-bullets="true"><\/swiper-container>');n[0].shadowEl.prepend($(`
+        <style>
+            .swiper-pagination-bullet-active {
+                background-color: var(--main-dark-color) !important;
+            }
+        </style>
+        `)[0]);$.get("src/img/"+t+"/index.json",t=>{t.forEach(t=>{n.append($(`<swiper-slide><img src='${t}'></img></swiper-slide>`))})});this.appendChild(n[0])}}customElements.define("slide-show",SlideShow)
