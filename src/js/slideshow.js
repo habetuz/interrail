9866117fe5ff67056a298af8f5613d6069b0d960
Marvin Fuchs <info@marvin-fuchs.de>
2023-05-17 10:48:44 +0200

    Update site

--- src/js/slideshow.js
+++ src/js/slideshow.js
@@ -1,7 +1,8 @@
-class SlideShow extends HTMLElement{connectedCallback(){var t=this.getAttribute("src"),n=$('<swiper-container mousewheel="true" autoplay-delay="2000" grab-cursor="true" space-between="50" pagination-clickable="true" pagination-dynamic-bullets="true"><\/swiper-container>');n[0].shadowEl.prepend($(`
+class SlideShow extends HTMLElement{connectedCallback(){var t=this.getAttribute("src"),n=$('<swiper-container mousewheel="true" autoplay-delay="2000" space-between="50" pagination-clickable="true" pagination-dynamic-bullets="true"><\/swiper-container>');n[0].shadowEl.prepend($(`
         <style>
-            .swiper-pagination-bullet-active {
+            .swiper-pagination-bullet {
                 background-color: var(--main-dark-color) !important;
+                border: 2px solid var(--main-color) !important;
             }
         </style>
         `)[0]);$.get("src/img/"+t+"/index.json",t=>{t.forEach(t=>{n.append($(`<swiper-slide><img src='${t}'></img></swiper-slide>`))})});this.appendChild(n[0])}}customElements.define("slide-show",SlideShow)
